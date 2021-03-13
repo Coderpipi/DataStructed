151d45f8d2c0bc1311510606d4467aeca05163e2
Coderpipi <2582482991@qq.com>
2021-03-13 10:40:03 +0800

    增加了堆排序的实现

--- include/sort.h
+++ include/sort.h
@@ -9,19 +9,30 @@ typedef struct {
 
 // 1.直接插入排序
 void InsertSort(RecType[], int);
+
 // 2.折半插入排序
 void BinInsertSort(RecType[], int);
+
 // 3.希尔排序
 void ShellSort(RecType[], int);
+
 // 4.冒泡排序
 void BubbleSort(RecType[], int);
+
 // 5.改进冒泡排序
 void BubbleSortWithFlag(RecType[], int);
+
 // 6.快速排序
 void QuickSort(RecType[], int, int);
+
 // 7.简单选择排序
 void SelectSort(RecType[], int);
+
+// 构建大根堆
+void sift(RecType[], int, int);
+
 // 8.堆排序
+void HeapSort(RecType[], int);
 
 // 9.归并排序
 void MergeSort(RecType[], int, int, int);
